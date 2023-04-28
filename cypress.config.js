335ee74a14af8b309c6e64e6c44e91dcd69384b0
Ahmed Alsaab <ahmed_alsaab@hotmail.com>
2023-04-28 10:11:44 +0100

    add athena and aws imports and setup config. add athenaExpress cypress task to send queries.

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,51 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require('cypress');
+const aws = require('aws-sdk');
+const AthenaExpress = require('athena-express');
+
+/* 
+ALTERNATIVE AUTH IF NOT USING TEMP CREDENTIALS FROM ~s3 DIRECTORY ON LOCAL MACHINE:
+
+const awsCredentials = {
+     region: YOUR DESIRED REGION,
+     accessKeyId: YOUR ACCESS KEY,
+     secretAccessKey: YOUR SECRET ACCESS KEY
+ };
+
+  aws.config.update(awsCredentials);
+*/
+
+aws.config.update({ region: 'eu-west-1' }); // ADD YOUR REGION
+
+const athenaExpressConfig = {
+  aws,
+  s3: ' ' /* YOUR S3 BUCKET PATH */,
+  db: '' /* YOUR ATHENA DB */,
+};
+
+const athenaExpress = new AthenaExpress(athenaExpressConfig);
 
 module.exports = defineConfig({
+  defaultCommandTimeout: 70000,
+  responseTimeout: 90000,
+
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      on('task', {
+        sendAthenaQuery(queryToSend) {
+          return new Promise((resolve, reject) => {
+            athenaExpress
+              .query(queryToSend)
+              .then((results) => {
+                console.log('Query success!');
+                resolve(results);
+              })
+              .catch((error) => {
+                console.log(error);
+                reject(error);
+              });
+          });
+        },
+      });
     },
   },
 });
